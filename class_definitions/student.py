989a085bd8d6d9984672d930c8d9ba78e236962e
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-10-27 10:02:16 -0500

    Final push - all tests pass

--- class_definitions/student.py
+++ class_definitions/student.py
@@ -2,14 +2,22 @@
 Author: Tyler Hochstetler
 The purpose of this class is to provide data for unit testing in the test.test_student.py file
 """
+import math
 
 
 class Student:
   """Student Class"""
   def __init__(self, lname, fname, major='', gpa=0.0):
+    name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
+    if not (name_characters.issuperset(lname) and name_characters.issuperset(fname) and name_characters.issuperset(major)):
+      raise ValueError
     self.last_name = lname
     self.first_name = fname
     self.major = major
+    if (math.isnan(gpa)):
+      raise TypeError
+    elif not (isinstance(gpa, float)):
+      raise ValueError
     self.gpa = gpa
   
   def __str__(self):
--- test/test_student.py
+++ test/test_student.py
@@ -10,25 +10,44 @@ class MyTestClass(unittest.TestCase):
     del self.student
   
   def test_object_created_required_attributes(self):
-    pass
+    self.assertEqual(self.student.last_name, 'Test')
+    self.assertEqual(self.student.first_name, 'Johnny')
 
   def test_object_created_all_attributes(self):
-    pass
+    student = s.Student('Bright', 'Gil', 'HVAC', 3.9)
+    self.assertEqual(student.last_name, 'Bright')
+    self.assertEqual(student.first_name, 'Gil')
+    self.assertEqual(student.major, 'HVAC')
+    self.assertEqual(student.gpa, 3.9)
 
   def test_student_str(self):
-    pass
+    gil = s.Student('Bright', 'Gil', 'HVAC', 3.9)
+    # print(str(gil)) # This was just to make sure it actually printed as expected
+    self.assertEqual(str(gil), "Bright, Gil has major HVAC with gpa: 3.9")
 
   def test_object_not_created_error_last_name(self):
-    pass
+    with self.assertRaises(ValueError):
+      t = s.Student('321', 'Jimmy')
 
   def test_object_not_created_error_first_name(self):
-    pass
+    with self.assertRaises(ValueError):
+      t = s.Student('Good', '987')
 
   def test_object_not_created_error_major(self):
-    pass
+    with self.assertRaises(ValueError):
+      t = s.Student('Good', 'Jimmy', '!Communication')
 
   def test_object_not_created_error_gpa(self):
-    pass
+    with self.assertRaises(TypeError):
+      t = s.Student('Phillips', 'Phil', 'CSI', 'A')
+    # with self.assertRaises(ValueError):
+    #   u = s.Student('Doe', 'Jane', 'Tech', -1)
+    # with self.assertRaises(ValueError):
+    #   v = s.Student('Marsh', 'Slug', 'Diesel', 4.1)
+  
+  def test_object_not_created_error_gpa_with_int(self):
+    with self.assertRaises(ValueError):
+      u = s.Student('Doe', 'Jane', 'Tech', -1)
 
 if __name__ == "__main__":
   unittest.main()
